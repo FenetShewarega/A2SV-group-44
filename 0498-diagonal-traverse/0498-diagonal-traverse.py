022d4c39684b89119907c81130bbba7b098c227c
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-01-04 18:33:39 +0300

    Time: 208 ms (71.24%), Space: 17.1 MB (76.43%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,32 +1,28 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        
-        directions = [(-1, 1), (1, -1)]
-        curr_direction = 0
-        n, m = len(mat), len(mat[0])
-        element_count = 0
-        
-        ans = []
-        curr_row, curr_col = 0, 0
-        in_bound = lambda x, y : 0 <= x < n and 0 <= y < m
-        
-        while element_count < n * m:
-            dx, dy = directions[curr_direction]
-            if in_bound(curr_row, curr_col):
-                ans.append(mat[curr_row][curr_col])
-                curr_row, curr_col = curr_row + dx, curr_col + dy
-                element_count += 1
-                
-            else:
-                if curr_col >= m:
-                    curr_row, curr_col = curr_row - dx + 1, curr_col - dy
-                elif curr_row >= n:
-                    curr_row, curr_col = curr_row - dx, curr_col - dy + 1
-                elif curr_row < 0:
-                    curr_row, curr_col = curr_row - dx, curr_col - dy + 1
-                elif curr_col < 0:
-                    curr_row, curr_col = curr_row - dx + 1, curr_col - dy
-                
-                curr_direction = 1 - curr_direction
+        def diagonalgiven(m, n):
+            diag = []
+            while m < len(mat[0]) and n >= 0:
+                diag .append(mat[n][m])
+                n-=1
+                m+=1
+            return diag    
             
-        return ans
+        out_put=[]
+        di = []
+        for row in range(len(mat)):
+            
+            diagonal = diagonalgiven(0,row)
+            di.append(diagonal)
+          
+        for col in range(1,len(mat[0])):
+            
+            diagonal_ = diagonalgiven(col,len(mat)-1)
+            di.append(diagonal_)
+            
+        for i in range(len(di)):
+            if i%2 == 1:
+                di[i].reverse()
+            out_put.extend(di[i])
+    
+        return out_put        
