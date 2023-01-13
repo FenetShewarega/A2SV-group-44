5b33e04fe56622b3cfab88179a4aed573206ec33
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-01-13 19:10:05 +0300

    Time: 207 ms (62.38%), Space: 14.4 MB (85.32%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.py
@@ -4,14 +4,15 @@ class Solution:
         n = len(grid) 
         maxlocal = [[0 for i in range(n - 2)] for i in  range(n-2)]
         max_= 0 
-      
+        directions = [(0, 0), (0, 1), (0, 2), 
+                      (1, 0), (1, 1), (1, 2), 
+                      (2, 0), (2, 1), (2, 2)]
+        
         for i in range(len(maxlocal)):
             for j in range(len(maxlocal)):
-                u = max(grid[i][j] , grid[i][j+1] , grid[i][j+2] )
-                m = max(grid[i+1][j],  grid[i+1][j+1] , grid[i+1][j+2] )
-                l = max(grid[i+2][j],  grid[i+2][j+1] , grid[i+2][j+2] )
-                val = max(u,m,l)
-              
+                val = -inf
+                for dx, dy in directions:
+                    val = max(val, grid[i + dx][j + dy])
                 maxlocal[i][j] = val
         return maxlocal
             
