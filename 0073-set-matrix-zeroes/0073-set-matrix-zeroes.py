5b9baf1027d6826ab6946683751a0e07e76aa417
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-02-07 18:14:48 +0300

    Time: 145 ms (37.82%), Space: 14.8 MB (27.21%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,21 +4,31 @@ class Solution:
  
         r=len(matrix)              
         c=len(matrix[0])
-        row=[0]*r
-        col=[0]*c
+        matrix_ = []
+        for i in range(r):
+            temp = []
+            for j in range(c):
+                temp.append(matrix[i][j])
+            matrix_.append(temp)    
+      
+              
+        for i in range(r): 
+            for j in range(c):
+                if matrix[i][j] == 0:
+
+                    left = 0 
+                    right = 0
+                    while left < c :
+                        matrix_[i][left] = 0
+                        left += 1
+                    while right < r :   
+                        matrix_[right][j] = 0
+                        right += 1
+
         for i in range(r):
             for j in range(c):
-                if matrix[i][j]==0:
-                    row[i]=1
-                    col[j]=1
-        for i in range(r):
-            if row[i]==1:
-                for j in range(c):
-                    matrix[i][j]=0
-        for i in range(c):
-            if col[i]==1:
-                for j in range(r):
-                    matrix[j][i]=0
+                matrix[i][j] = matrix_[i][j]
+        
                     
                     
         
