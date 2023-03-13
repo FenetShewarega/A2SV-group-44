61bb5eba40c027a4c5e18394333bbda063af83ba
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-03-13 23:12:48 +0300

    Time: 8677 ms (5.05%), Space: 13.8 MB (98.16%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -8,10 +8,9 @@ class Solution:
             if  left > right :
                 return 0
         
-            # print( helper(left + 1 ,  right))
             choiceLeft = nums[left] - helper(left + 1 ,  right)
             choiceRight = nums[right] - helper(left , right - 1)
-            # print(choiceLeft)
+            
             return (max(choiceLeft , choiceRight))
 
         return helper( 0, N - 1) >= 0
