3849117534108f015c91f9e455f7b2453eba63de
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-03-16 00:23:55 +0300

    Time: 658 ms (35.20%), Space: 20 MB (5.39%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -9,18 +9,13 @@ class Solution:
             mid = (r + l)//2
             t = 1
             total = 0
-            for i in w:
-                total += i
+            for i in range(len(w)):
+                total += w[i]
                 if total > mid:
                     t += 1
-                    total = i
-            
+                    total = w[i]
             if t > days:
-          
                 l = mid + 1
-            
             else:
-             
                 r = mid - 1
-                
         return l
