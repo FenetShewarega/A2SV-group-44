f7053e7146a9106807c92a68af4392f17d59435d
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-03-18 01:55:59 +0300

    Time: 34 ms (65.74%), Space: 13.9 MB (35.36%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        result1 = self.traverseRoot1(root1 , [])
+        result2 = self.traverseRoot2(root2, [])
+        if result1 == result2:
+            return True
+        else:
+            return False
+        
+        
+    def traverseRoot1(self,root ,path1):
+        
+        
+        if not root :
+            return []
+        if  not root.left and not root.right:
+            path1.append(root.val)
+        else:
+            self.traverseRoot1(root.left , path1)
+            self.traverseRoot1(root.right , path1)
+        return path1
+    def traverseRoot2(self,root , path2):
+        if not root :
+            return []
+        if  not root.left and not root.right:
+            path2.append(root.val)
+        else:
+            self.traverseRoot2(root.left , path2)
+            self.traverseRoot2(root.right , path2)
+        return path2
+        
+        
