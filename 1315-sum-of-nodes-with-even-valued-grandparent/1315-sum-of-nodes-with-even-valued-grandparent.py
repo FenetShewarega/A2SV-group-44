d5dc1906942c3c24cd4778e6e99fa094e57fe9f2
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-04-28 18:05:25 +0300

    Time: 121 ms (11.68%), Space: 21 MB (7.16%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -1,75 +1,18 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def sumEvenGrandparent(self, root: TreeNode) -> int:
-        
-        
-        def dfs( node, par_node , grad_node):
+        ans = 0
+        queue = deque([(root, TreeNode(-1), TreeNode(-1))])
+        while queue:
+            curr_node, par_node, grand_node = queue.popleft()
+            # print(curr_node.val, par_node.val, grand_node.val)
             
-            if not node :
-                return 0
-            
-            tot = 0
-            if grad_node.val % 2 == 0:
-                tot += node.val
-            
-            return tot + dfs(node.left,node ,par_node) + dfs(node.right , node,par_node)
-            
-        return dfs(root, TreeNode(-1), TreeNode(-1))
-            
-            
-       
-     
-#         self.tot = 0
-#         self.travers(root)
-#         return self.tot
-#     def travers(self,root):
-#         if not root:
-#             return
-      
-        
-#         if root.val % 2 == 0:
-            
-#             if root.left and root.left.right :
-#                 self.tot+= root.left.right.val
-#             if root.left and root.left.left:
-#                 self.tot += root.left.left.val
-#             if root.right and root.right.right :
-#                 self.tot+= root.right.right.val
-#             if root.right and root.right.left:
-#                 self.tot+= root.right.left.val
-#         self.travers(root.left)
-#         self.travers(root.right)
-            
-            
-        
-        
-        
-        
-# #         self.path = []
-# #         self.paths = []
-        
-# #         self.travers(root,path)
-# #         for i in paths:
-# #             for j in range(len(i)):
+            if grand_node.val % 2 == 0:
+                ans += curr_node.val
+            if curr_node.left:
+                queue.append((curr_node.left, curr_node, par_node))
                 
-                
+            if curr_node.right:
+                queue.append((curr_node.right, curr_node, par_node))
         
-# #     def travers(self, root , path):
-# #         if not root:
-# #             return []
-# #         path.append(root.val)
-# #         if not root.left and not root.right:
-# #             self.paths.append(path)
-            
-# #         else:
-# #             if root.left:
-# #                 self.travers(root.left ,path)
-# #             if root.right:
-# #                 self.travers(root.right ,path)
-           
-# #         return  (self.paths)
+        return ans
+        
