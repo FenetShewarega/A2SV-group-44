74d392cf952cacf2ce580fa713b90cf03a0e9ea1
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-04-28 17:41:53 +0300

    Time: 131 ms (6.66%), Space: 20 MB (7.16%) - LeetHub

---
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -0,0 +1,60 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        
+       
+     
+        self.tot = 0
+        self.travers(root)
+        return self.tot
+    def travers(self,root):
+        if not root:
+            return
+      
+        
+        if root.val % 2 == 0:
+            
+            if root.left and root.left.right :
+                self.tot+= root.left.right.val
+            if root.left and root.left.left:
+                self.tot += root.left.left.val
+            if root.right and root.right.right :
+                self.tot+= root.right.right.val
+            if root.right and root.right.left:
+                self.tot+= root.right.left.val
+        self.travers(root.left)
+        self.travers(root.right)
+            
+            
+        
+        
+        
+        
+#         self.path = []
+#         self.paths = []
+        
+#         self.travers(root,path)
+#         for i in paths:
+#             for j in range(len(i)):
+                
+                
+        
+#     def travers(self, root , path):
+#         if not root:
+#             return []
+#         path.append(root.val)
+#         if not root.left and not root.right:
+#             self.paths.append(path)
+            
+#         else:
+#             if root.left:
+#                 self.travers(root.left ,path)
+#             if root.right:
+#                 self.travers(root.right ,path)
+           
+#         return  (self.paths)
