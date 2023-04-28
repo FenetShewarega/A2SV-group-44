d096c497b41121fb5df86336b41e1e4168bc33fe
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-04-28 17:53:23 +0300

    Time: 110 ms (46.86%), Space: 20.9 MB (7.16%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.py
@@ -7,54 +7,69 @@
 class Solution:
     def sumEvenGrandparent(self, root: TreeNode) -> int:
         
+        
+        def dfs( node, par_node , grad_node):
+            
+            if not node :
+                return 0
+            
+            tot = 0
+            if grad_node.val % 2 == 0:
+                tot += node.val
+            
+            return tot + dfs(node.left,node ,par_node) + dfs(node.right , node,par_node)
+            
+        return dfs(root, TreeNode(-1), TreeNode(-1))
+            
+            
        
      
-        self.tot = 0
-        self.travers(root)
-        return self.tot
-    def travers(self,root):
-        if not root:
-            return
+#         self.tot = 0
+#         self.travers(root)
+#         return self.tot
+#     def travers(self,root):
+#         if not root:
+#             return
       
         
-        if root.val % 2 == 0:
+#         if root.val % 2 == 0:
             
-            if root.left and root.left.right :
-                self.tot+= root.left.right.val
-            if root.left and root.left.left:
-                self.tot += root.left.left.val
-            if root.right and root.right.right :
-                self.tot+= root.right.right.val
-            if root.right and root.right.left:
-                self.tot+= root.right.left.val
-        self.travers(root.left)
-        self.travers(root.right)
+#             if root.left and root.left.right :
+#                 self.tot+= root.left.right.val
+#             if root.left and root.left.left:
+#                 self.tot += root.left.left.val
+#             if root.right and root.right.right :
+#                 self.tot+= root.right.right.val
+#             if root.right and root.right.left:
+#                 self.tot+= root.right.left.val
+#         self.travers(root.left)
+#         self.travers(root.right)
             
             
         
         
         
         
-#         self.path = []
-#         self.paths = []
+# #         self.path = []
+# #         self.paths = []
         
-#         self.travers(root,path)
-#         for i in paths:
-#             for j in range(len(i)):
+# #         self.travers(root,path)
+# #         for i in paths:
+# #             for j in range(len(i)):
                 
                 
         
-#     def travers(self, root , path):
-#         if not root:
-#             return []
-#         path.append(root.val)
-#         if not root.left and not root.right:
-#             self.paths.append(path)
+# #     def travers(self, root , path):
+# #         if not root:
+# #             return []
+# #         path.append(root.val)
+# #         if not root.left and not root.right:
+# #             self.paths.append(path)
             
-#         else:
-#             if root.left:
-#                 self.travers(root.left ,path)
-#             if root.right:
-#                 self.travers(root.right ,path)
+# #         else:
+# #             if root.left:
+# #                 self.travers(root.left ,path)
+# #             if root.right:
+# #                 self.travers(root.right ,path)
            
-#         return  (self.paths)
+# #         return  (self.paths)
