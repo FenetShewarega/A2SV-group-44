7cfcb356930422948a46087c224268d03620c843
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-05-31 18:20:16 +0300

    Time: 55 ms (8.25%), Space: 16.3 MB (21.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,21 +1,18 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        
-        self.store = defaultdict(tuple)
-        return self.pathtracker(0 , 0 , m - 1, n - 1)
+        store = [ [0 for i in range (n) ]  for j in range(m) ]
+        store[m-1][n-1] = 1
+        for i in  range(  m , -1 , -1):
+            for j in range ( n , -1 , -1):
+                if i < m - 1 and j == n - 1:
+                    store[i][j] += store[i + 1][j]
+                if j < n - 1  and  i == m -1:
+                    store[i][j] += store[i][j+1]
+                if j < n - 1 and i < m - 1:
+                    store[i][j] +=  store[i + 1][j] + store[i][j+1]
+                
+                
+        # print (store)
+        return store[0][0]    
     
-    def pathtracker(self,i , j , m , n):
-        
-        if i == m and j == n:
-            return 1
-        if (i , j) not in self.store:
-            
-            if i == m and j < n :
-                self.store[(i , j)] = self.pathtracker(i  , j + 1 , m, n )
-            if j == n and i < m:
-                self.store[(i , j)] = self.pathtracker(i  + 1, j , m, n )
-            if j < n and i < m:
-                self.store[(i , j)] = self.pathtracker(i+1 ,j , m , n) + self.pathtracker(i , j + 1, m ,n)
-
-        return self.store[(i , j)]
