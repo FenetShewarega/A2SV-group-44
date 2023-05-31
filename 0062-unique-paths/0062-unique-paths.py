705f6684b2e375bfb10a5e0f9f6d05e8c96016d2
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-05-31 18:25:39 +0300

    Time: 60 ms (6.41%), Space: 16.2 MB (43.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,18 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
+        
         store = [ [0 for i in range (n) ]  for j in range(m) ]
-        store[m-1][n-1] = 1
-        for i in  range(  m , -1 , -1):
-            for j in range ( n , -1 , -1):
-                if i < m - 1 and j == n - 1:
-                    store[i][j] += store[i + 1][j]
-                if j < n - 1  and  i == m -1:
-                    store[i][j] += store[i][j+1]
-                if j < n - 1 and i < m - 1:
-                    store[i][j] +=  store[i + 1][j] + store[i][j+1]
+        store[0][0] = 1
+        for i in  range(  m ):
+            for j in range ( n ):
+                if i > 0 and j == 0:
+                    store[i][j] += store[i -1 ][j]
+                if j > 0  and  i == 0:
+                    store[i][j] += store[i][j-1]
+                if j > 0 and i > 0:
+                    store[i][j] +=  store[i - 1][j] + store[i][j-1]
                 
                 
         # print (store)
-        return store[0][0]    
+        return store[-1][-1]    
     
