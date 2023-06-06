69909042355fc9149ebb9a55098be07d2df90dfb
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-06-06 23:11:29 +0300

    Time: 1726 ms (33.79%), Space: 197.4 MB (5.88%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        
+        n = len(questions)
+        @cache
+        def dp(i):
+            
+            
+            if i >= n:
+                return 0
+            return max(dp(i + questions[i][1] + 1) + questions[i][0]  ,dp(i + 1))
+        return dp( 0 )
+        
