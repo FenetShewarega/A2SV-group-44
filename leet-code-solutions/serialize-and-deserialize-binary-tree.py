fa97bdb7f21c37009aa6ea1342ecd7363e2f650b
FenetShewarega <88624737+FenetShewarega@users.noreply.github.com>
2023-11-06 09:00:07 +0000

    Added solution for Serialize and Deserialize Binary Tree

---
+++ leet-code-solutions/serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        res = []
+        def dfs(node):
+            if not node:
+                res.append('x')
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ' '.join(res)
+
+    def deserialize(self, data):
+        arr = data.split()
+        def dfs():
+            val = arr.pop(0)
+            if val == 'x':
+                return None
+            node = TreeNode(int(val))
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
